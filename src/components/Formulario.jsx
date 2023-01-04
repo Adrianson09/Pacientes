75b6c9be0f78052539a4410442198961eacfb08f
Adrian Guerrero <adrianson@outlook.com>
2023-01-04 10:43:37 -0600

    Add useState form

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,16 +1,27 @@
 import { useState, useEffect } from "react"
 
 function Formulario() {
+  const [nombre, setNombre] = useState('');
+  const [email, setEmail] = useState('');
+  const [telefono, setTelefono] = useState('');
+  const [fecha, setFecha] = useState('');
+  const [sintomas, setSintomas] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Enviando...')
+  }
+
   return (
-    <div className="md:w-1/2 lg:w-2/5">
+    <div className="md:w-1/2 lg:w-2/5 mx-5 ">
           <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
           <p className="text-lg mt-5 text-center mb-10">Añade Pacientes y {''} 
           <span className="text-cyan-600 font-bold ">Administralos</span>
           </p>
-          <form className="bg-white shadow-md mb-10 rounded-lg py-10 px-5">
+          <form className="bg-white shadow-md mb-10 rounded-lg py-10 px-5" onSubmit={handleSubmit}>
               <div className="mb-5">
                 <label htmlFor="nombre" className="block text-gray-700 uppercase font-bold"> Nombre</label>
-                <input className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" type="text" name="" id="nombre" placeholder="Nombre del Paciente"/>
+                <input className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" type="text" value={nombre} onChange={ (e) => setNombre(e.target.value) } id="nombre" placeholder="Nombre del Paciente"/>
               </div>
 
               {/* <div className="mb-5">
@@ -20,22 +31,22 @@ function Formulario() {
 
               <div className="mb-5">
                 <label htmlFor="email" className="block text-gray-700 uppercase font-bold"> Email</label>
-                <input className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" type="email" name="" id="email" placeholder="Email del Paciente"/>
+                <input className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" type="email" name="" id="email" placeholder="Email del Paciente" value={email} onChange={ (e) => setEmail(e.target.value) }/>
               </div>
 
               <div className="mb-5">
                 <label htmlFor="telefono" className="block text-gray-700 uppercase font-bold"> Teléfono</label>
-                <input className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" type="tel" name="" id="telefono" placeholder="Teléfono del Paciente"/>
+                <input className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" type="tel" name="" id="telefono" placeholder="Teléfono del Paciente" value={telefono} onChange={ (e) => setTelefono(e.target.value) }/>
               </div>
 
               <div className="mb-5">
                 <label htmlFor="fecha" className="block text-gray-700 uppercase font-bold"> Fecha</label>
-                <input className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" type="datetime-local" name="" id="fecha" />
+                <input className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" type="datetime-local" name="" id="fecha" value={fecha} onChange={ (e) => setFecha(e.target.value) }/>
               </div>
 
               <div className="">
                 <label htmlFor="sintomas" className="block text-gray-700 uppercase font-bold"> Síntomas</label>
-                <textarea rows="3" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" type="text-area" name="" id="sintomas" placeholder="Describe los síntomas"/>
+                <textarea rows="3" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" type="text-area" name="" id="sintomas" placeholder="Describe los síntomas" value={sintomas} onChange={ (e) => setSintomas(e.target.value) }/>
               </div>
               <input type="submit"
               className="bg-cyan-600 hover:bg-cyan-700 cursor-pointer transition-colors w-full p-3 uppercase text-white font-bold"
--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -2,7 +2,7 @@
 
 const Paciente = () => {
   return (
-    <div className=" bg-white shadow-md m-3  py-10 px-5  rounded-xl">
+    <div className=" bg-white shadow-md mx-5 my-10  py-10 px-5  rounded-xl">
         <p className="font-bold mb-3 text-gray-700 uppercase"> Nombre: 
                   <span className="font-normal normal-case"> Adrian
                   </span>  </p>
