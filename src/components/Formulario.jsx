ec36ba1567ae1545a4e071feabaf049bd6988acf
Adrian Guerrero <adrianson@outlook.com>
2023-01-04 13:12:36 -0600

    Add error message if input is empty

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -7,14 +7,18 @@ function Formulario() {
   const [fecha, setFecha] = useState('');
   const [sintomas, setSintomas] = useState('');
 
+  const [error, setError] = useState(false);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     // Validar Formulario
     if( [nombre, email, telefono, fecha, sintomas ].includes('') ){
-      console.log('Hay campos vacios')
-    } else{
-      console.log('Todos los campos llenos')
-    }
+      
+      setError(true)
+      return;
+    } 
+
+    setError(false)
   }
 
   return (
@@ -24,6 +28,11 @@ function Formulario() {
           <span className="text-cyan-600 font-bold ">Administralos</span>
           </p>
           <form className="bg-white shadow-md mb-10 rounded-lg py-10 px-5" onSubmit={handleSubmit}>
+            {error && 
+              <div className="mb-3 p-3 text-center bg-red-800 text-white font-bold uppercase rounded">
+                  <p >Todos los campos son obligatorios!</p>
+              </div>
+            }
               <div className="mb-5">
                 <label htmlFor="nombre" className="block text-gray-700 uppercase font-bold"> Nombre</label>
                 <input className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" type="text" value={nombre} onChange={ (e) => setNombre(e.target.value) } id="nombre" placeholder="Nombre del Paciente"/>
