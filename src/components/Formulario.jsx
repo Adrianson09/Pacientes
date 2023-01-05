16214f87ef8c9c5690f255d862450ccb245e1ee0
Adrian Guerrero <adrianson@outlook.com>
2023-01-05 17:48:15 -0600

    Add Buttons Edit and Delete

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -10,6 +10,12 @@ function Formulario({ pacientes, setPacientes }) {
 
   const [error, setError] = useState(false);
 
+  const generarId = () => {
+    const random = Math.random().toString(36).substr(2);
+    const fecha = Date.now().toString(36)
+    return fecha + random
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault();
     // Validar Formulario
@@ -27,7 +33,8 @@ function Formulario({ pacientes, setPacientes }) {
       email,
       telefono,
       fecha,
-      sintomas
+      sintomas,
+      id: generarId()
     }
 
     setPacientes([...pacientes, objetoPaciente]);
--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -6,21 +6,31 @@ const ListadoPacientes = ({pacientes}) => {
 
   return (
     <div className="md:w-1/2 lg:w-3/5 md:h-screen md:overflow-y-scroll">
-          <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
-          <p className="text-xl mt-5 text-center mb-10">Administra tus {''}
-          <span className="text-cyan-600 font-bold">Pacientes y Citas</span></p>
-
-          <div className=" ">
-
-                { pacientes.map( (paciente, index) => (
-                  <Paciente
-                    key={index}
-                    paciente={paciente}
-                  />
-                ))}
+          {pacientes && pacientes.length ? (
+            <>
+                  <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
+                  <p className="text-xl mt-5 text-center mb-10">Administra tus {''}
+                  <span className="text-cyan-600 font-bold">Pacientes y Citas</span></p>
+   
+                   { pacientes.map( paciente  => (
+                     <Paciente
+                       key={paciente.id}
+                       paciente={paciente}
+                     />
+                   ))}
+             </> 
+                  ) : (
+                    <>
+                      <h2 className="font-black text-3xl text-center">Agrega Pacientes</h2>
+                      <p className="text-xl mt-5 text-center mb-10">Comienza agregando  {''}
+                      <span className="text-cyan-600 font-bold">Pacientes </span></p>
+                    
+                    </>
+                  )}
+                   
                 
                
-          </div>
+          
     </div>
     
   )
